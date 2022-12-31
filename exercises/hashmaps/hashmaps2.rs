4ba031e41d9a78d12ea222d64c972b25ec704d4b
Daniel Naschberger <daniel@naschi.at>
2022-12-31 20:34:23 +0100

    🦀: hashmaps

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,6 +35,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.insert(Fruit::Apple, 4);
+        basket.insert(Fruit::Mango, 2);
+        basket.insert(Fruit::Lychee, 5);
+        basket.insert(Fruit::Banana, 4);
+        basket.insert(Fruit::Pineapple, 6);
+        if basket.contains_key(&fruit) {
+            continue;
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,16 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let team_1 = Team { name: team_1_name.to_owned(), goals_scored: 0, goals_conceded: 0};
+        let score_1 = scores.entry(team_1_name).or_insert(team_1);
+        score_1.goals_scored += team_1_score;
+        score_1.goals_conceded += team_2_score;
+
+        let team_2 = Team { name: team_2_name.to_owned(), goals_scored: 0, goals_conceded: 0};
+        let score_2 = scores.entry(team_2_name).or_insert(team_2);
+        score_2.goals_scored += team_2_score;
+        score_2.goals_conceded += team_1_score;
     }
     scores
 }
